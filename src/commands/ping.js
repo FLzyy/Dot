9f384aa423621303850e62f1c0644e3e9ed52e3d
FLzyy <96031953+FLzyy@users.noreply.github.com>
2022-12-12 12:36:13 +0000

    Initialize project

---
+++ src/commands/ping.js
@@ -0,0 +1,10 @@
+import { SlashCommandBuilder } from "discord.js"
+
+export let command = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply(`🏓 Pong! ${Date.now() - interaction.createdTimestamp}`);
+	},
+}

b2a17734e10ecbec42d38454fb2b1d546b168979
FLzyy <96031953+FLzyy@users.noreply.github.com>
2022-12-13 06:05:09 +0000

    registering

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,10 +1,12 @@
-import { SlashCommandBuilder } from "discord.js"
+import { SlashCommandBuilder } from 'discord.js';
 
 export let command = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply(`🏓 Pong! ${Date.now() - interaction.createdTimestamp}`);
-	},
-}
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with Pong!'),
+    async execute(interaction) {
+        await interaction.reply(
+            `🏓 Pong! ${Date.now() - interaction.createdTimestamp}ms`
+        );
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,37 +1,35 @@
-import * as dotenv from "dotenv"
-dotenv.config()
-const token = process.env.DISCORD_TOKEN
+import * as dotenv from 'dotenv';
+dotenv.config();
+const token = process.env.DISCORD_TOKEN;
 
 import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
-import { readdir } from "node:fs/promises";
-import { join, dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-const __dirname = dirname(fileURLToPath(import.meta.url))
+import { readdir } from 'node:fs/promises';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 
-const commandsPath = join(__dirname, "commands")
+const commandsPath = join(__dirname, 'commands');
 const commandFiles = await readdir(commandsPath);
-commandFiles.filter(file => file.endsWith('.js'));
+commandFiles.filter((file) => file.endsWith('.js'));
 
 const length = commandFiles.length;
 for (let i = 0; i < length; i++) {
-    const filePath = join(commandsPath, commandFiles[i])
-    const { command } = await import(filePath)
+    const filePath = join(commandsPath, commandFiles[i]);
+    const { command } = await import(filePath);
 
-    if ("data" in command && "execute" in command) {
-        client.commands.set(command.data.name, command)
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
     } else {
-        console.error(`Invalid Command: ${commandFiles[i]}`)
+        console.error(`Invalid Command: ${commandFiles[i]}`);
     }
-
-    
 }
- 
-client.once(Events.ClientReady, c => {
-	console.log(`${c.user.tag} is awake!`);
+
+client.once(Events.ClientReady, (c) => {
+    console.log(`${c.user.tag} is awake!`);
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
@@ -39,18 +37,22 @@ client.on(Events.InteractionCreate, async (interaction) => {
 
     const command = interaction.client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+    if (!command) {
+        console.error(
+            `No command matching ${interaction.commandName} was found.`
+        );
+        return;
+    }
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-})
-
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({
+            content: 'There was an error while executing this command!',
+            ephemeral: true
+        });
+    }
+});
 
 client.login(token);
